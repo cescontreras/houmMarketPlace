4ddec2ac22efe2f4306ded40859ec218961e887e
cescontreras <contreras-cesaralejandro@hotmail.com>
2020-12-25 01:05:01 -0300

    search

--- src/components/search/searchContainer.jsx
+++ src/components/search/searchContainer.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import image from "../../assets/back1.jpg"
+import "./searchContainer.css";
 
 export default function Search () {
 
   return (
-    <div className="search">
+    <div className="searchContainer">
+      {/* <img className="backImage" src={image}></img> */}
+      {/* <div className="content"> */}
       <h1>SEARCH</h1>
+
+      {/* </div> */}
     </div>
   )
 }
